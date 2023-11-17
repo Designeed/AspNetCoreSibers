bea834c61ad7105f5b9d17ef53a8002a044ce8e2
Burtsev Vlad <designeedgames@yandex.ru>
2023-11-17 22:45:44 +0700

    Реализованы репозитории для взаимодействия с бд

--- Domain/AppDbContext.cs
+++ Domain/AppDbContext.cs
@@ -7,18 +7,21 @@ namespace AspNetCoreSibers.Domain
 {
     public class AppDbContext : IdentityDbContext<IdentityUser>
     {
-        public AppDbContext(DbContextOptions<AppDbContext> option) : base(option) { }
+        public AppDbContext(DbContextOptions options) : base(options)
+        {
+            Database.EnsureCreated();
+        }
 
         public DbSet<Employee> Employees { get; set; }
         public DbSet<Project> Projects { get; set; }
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
+            base.OnModelCreating(modelBuilder);
+
             modelBuilder.Entity<Project>()
                 .HasMany(e => e.Employees)
                 .WithMany(e => e.Projects);
-
-            base.OnModelCreating(modelBuilder);
         }
     }
 }
---
+++ Domain/Repositories/ProjectRepository/EFProjectRepository.cs
@@ -0,0 +1,43 @@
+﻿using AspNetCoreSibers.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace AspNetCoreSibers.Domain.Repositories.ProjectRepository
+{
+    public class EFProjectRepository : IProjectRepository
+    {
+        private readonly AppDbContext _dbContext;
+
+        public EFProjectRepository(AppDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        public async Task AddProjectAsync(Project project)
+        {
+            await _dbContext.Projects.AddAsync(project);
+            await _dbContext.SaveChangesAsync();
+        }
+
+        public async Task DeleteProjectAsync(Guid id)
+        {
+            _dbContext.Projects.Remove(new Project { Id = id });
+            await _dbContext.SaveChangesAsync();
+        }
+
+        public async Task EditProjectAsync(Project project)
+        {
+            _dbContext.Update(project);
+            await _dbContext.SaveChangesAsync();
+        }
+
+        public async Task<Project> GetProjectByIdAsync(Guid id)
+        {
+            return await _dbContext.Projects.FirstOrDefaultAsync(e => e.Id == id);
+        }
+
+        public async Task<IReadOnlyList<Project>> GetProjectsAsync()
+        {
+            return await _dbContext.Projects.ToListAsync();
+        }
+    }
+}
