7b131b3396393f69384b143d7fbcf8961db51c15
Burtsev Vlad <designeedgames@yandex.ru>
2023-11-18 23:12:43 +0700

    Создана страница списка сотрудников

---
+++ Controllers/EmployeeController.cs
@@ -0,0 +1,20 @@
+﻿using AspNetCoreSibers.Domain.Repositories.EmployeeRepository;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AspNetCoreSibers.Controllers
+{
+    public class EmployeeController : Controller
+    {
+        private readonly IEmployeeRepository _employeeRepository;
+
+        public EmployeeController(IEmployeeRepository employeeRepository)
+        {
+            _employeeRepository = employeeRepository;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
