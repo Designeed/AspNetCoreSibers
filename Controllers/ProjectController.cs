403b2e614ffce6f61c20e9f1212f3a612a31b382
Burtsev Vlad <designeedgames@yandex.ru>
2023-11-18 23:09:54 +0700

    Реализована функция создания проекта и удаления из БД

--- Controllers/ProjectController.cs
+++ Controllers/ProjectController.cs
@@ -1,6 +1,8 @@
 ﻿using AspNetCoreSibers.Domain;
 using AspNetCoreSibers.Domain.Entities;
+using AspNetCoreSibers.Domain.Repositories.EmployeeRepository;
 using AspNetCoreSibers.Domain.Repositories.ProjectRepository;
+using AspNetCoreSibers.Service;
 using Microsoft.AspNetCore.Mvc;
 using System.Collections;
 
@@ -9,9 +11,13 @@ namespace AspNetCoreSibers.Controllers
     public class ProjectController : Controller
     {
         private readonly IProjectRepository _projectRepository;
-        public ProjectController(IProjectRepository projectRepository)
+        private readonly IEmployeeRepository _employeeRepository;
+
+
+        public ProjectController(IProjectRepository projectRepository, IEmployeeRepository employeeRepository)
         {
             _projectRepository = projectRepository;
+            _employeeRepository = employeeRepository;
         }
 
         public async Task<IActionResult> Index()
@@ -21,10 +27,33 @@ namespace AspNetCoreSibers.Controllers
             return View(await taskProjectList);
         }
 
-
-        public IActionResult Add()
+        public IActionResult Create()
         {
             return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Create(Project project)
+        {
+            if (!ModelState.IsValid)
+                return View(project);
+
+            var taskCreationProject = _projectRepository.AddProjectAsync(project);
+
+            await taskCreationProject;
+
+            return RedirectToAction(nameof(ProjectController.Index), nameof(ProjectController).RemoveController());
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Delete(Guid id)
+        {
+            
+            var taskDeletionProject = _projectRepository.DeleteProjectAsync(id);
+
+            await taskDeletionProject;
+
+            return RedirectToAction(nameof(ProjectController.Index), nameof(ProjectController).RemoveController());
         }
     }
 }
