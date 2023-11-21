41b08f34777f2a3b77350ebf51c54510049732b0
Burtsev Vlad <designeedgames@yandex.ru>
2023-11-21 20:13:03 +0700

    Добавлена динамический вывод параметров соритровки с аннотациями

--- Service/Extantions.cs
+++ Service/Extantions.cs
@@ -1,8 +1,19 @@
-﻿namespace AspNetCoreSibers.Service
+﻿using System.ComponentModel.DataAnnotations;
+using System.Reflection;
+
+namespace AspNetCoreSibers.Service
 {
     public static class Extantions
     {
         public static string RemoveController(this string str)
             => str.Replace("Controller", string.Empty, StringComparison.OrdinalIgnoreCase);
+
+        public static string GetDisplayName(this Enum enumValue)
+            =>
+            enumValue.GetType()
+            .GetMember(enumValue.ToString())
+            .First()
+            .GetCustomAttribute<DisplayAttribute>()?
+            .GetName() ?? enumValue.ToString(); 
     }
 }
--- Service/Shared/ProjectSortType.cs
+++ Service/Shared/ProjectSortType.cs
@@ -1,10 +1,16 @@
-﻿namespace AspNetCoreSibers.Service.Shared
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace AspNetCoreSibers.Service.Shared
 {
     public enum ProjectSortType
     {
+        [Display(Name = "Имени")]
         Name,
+        [Display(Name = "Дате начала")]
         StartDate,
+        [Display(Name = "Дате окончания")]
         EndDate,
+        [Display(Name = "Приоритету")]
         Priority,
     }
 }
