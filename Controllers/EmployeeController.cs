8394609eaebf4b37378909a82c3c616802f0d190
Burtsev Vlad <designeedgames@yandex.ru>
2023-11-21 14:44:52 +0700

    Реализовано добавление, редактирование, удаление проектов и сотрудников

--- Controllers/EmployeeController.cs
+++ Controllers/EmployeeController.cs
@@ -1,5 +1,8 @@
-﻿using AspNetCoreSibers.Domain.Repositories.EmployeeRepository;
+﻿using AspNetCoreSibers.Domain.Entities;
+using AspNetCoreSibers.Domain.Repositories.EmployeeRepository;
+using AspNetCoreSibers.Service.Shared;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.CodeAnalysis.Differencing;
 
 namespace AspNetCoreSibers.Controllers
 {
@@ -12,9 +15,56 @@ namespace AspNetCoreSibers.Controllers
             _employeeRepository = employeeRepository;
         }
 
-        public IActionResult Index()
+        public async Task<IActionResult> Index()
+        {
+            var taskEmployeeList = _employeeRepository.GetEmployeesAsync();
+
+            return View(await taskEmployeeList);
+        }
+
+        public async Task<IActionResult> Delete(Guid id)
+        {
+            var taskDeletionEmployee = _employeeRepository.DeleteEmployeeByIdAsync(id);
+
+            await taskDeletionEmployee;
+
+            return RedirectToAction(nameof(EmployeeController.Index), nameof(EmployeeController).RemoveController());
+        }
+
+        public IActionResult Create()
         {
             return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Create(Employee employee)
+        {
+            if (!ModelState.IsValid)
+                return View(employee);
+
+            await _employeeRepository.AddEmployeeAsync(employee);
+
+            return RedirectToAction(nameof(EmployeeController.Index), nameof(EmployeeController).RemoveController());
+        }
+
+        public async Task<IActionResult> Edit(Guid id)
+        {
+            var employee = await _employeeRepository.GetEmployeeByIdAsync(id);
+
+            return View(employee);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Edit(Employee employee)
+        {
+            if (!ModelState.IsValid)
+                return View();
+
+            var taskEditionEmployee = _employeeRepository.EditEmployeeAsync(employee);
+
+            await taskEditionEmployee;
+
+            return RedirectToAction(nameof(EmployeeController.Index), nameof(EmployeeController).RemoveController());
         }
     }
 }
